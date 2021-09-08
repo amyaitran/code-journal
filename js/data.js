9671b414133ddb625c6e83decca60cf19ade3f66
Amy Tran <amyaitran@gmail.com>
2021-09-08 11:55:02 -0700

    changed margin classes, edited and relocated main.js code

--- js/data.js
+++ js/data.js
@@ -7,31 +7,6 @@ var data = {
   nextEntryId: 1
 };
 
-var $entry;
-var $photoimg = document.querySelector('#entry-img');
-var $photoURL = document.querySelector('#photo');
-var $form = document.querySelector('#input-form');
-
-$photoURL.addEventListener('input', handleInput);
-$form.addEventListener('submit', handleSubmit);
-
-function handleInput(event) {
-  $photoimg.setAttribute('src', event.target.value);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  var title = event.target.title.value;
-  var photo = event.target.photo.value;
-  var notes = event.target.notes.value;
-  $entry = { title, photo, notes };
-  $entry.EntryId = data.nextEntryId;
-  data.nextEntryId++;
-  data.entries.unshift($entry);
-  $photoimg.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $form.reset();
-}
-
 var previousEntriesJSON = localStorage.getItem('javascript-local-storage');
 
 if (previousEntriesJSON !== null) {
