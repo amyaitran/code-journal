33602256ac6b9c2c8716cfeac57bd8bb0263f64d
Amy Tran <amyaitran@gmail.com>
2021-09-08 17:05:55 -0700

    added css for new entries, created DOM tree for new entries

--- js/main.js
+++ js/main.js
@@ -24,3 +24,62 @@ function handleSubmit(event) {
   $photoimg.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 }
+
+// DOM tree
+// <div class="row">
+//   <div class="column-full column-half">
+//     <img src="images/placeholder-image-square.jpg">
+//   </div>
+
+//   <div class="column-full column-half">
+//     <div class="row">
+//       <h2 class="mb-0">Title</h2>
+//     </div>
+
+//     <div class="row">
+//       <p>Notes Lorem ipsum </p>
+//     </div>
+//   </div>
+
+function renderEntry(entry) {
+  var divRow = document.createElement('div');
+  var divRow2 = document.createElement('div');
+  var divRow3 = document.createElement('div');
+  var divColFullColHalf = document.createElement('div');
+  var divColFullColHalf2 = document.createElement('div');
+  var img = document.createElement('img');
+  var h2 = document.createElement('h2');
+  var p = document.createElement('p');
+
+  divRow.setAttribute('class', 'row');
+  divRow2.setAttribute('class', 'row');
+  divRow3.setAttribute('class', 'row');
+  divColFullColHalf.setAttribute('class', 'column-full column-half mb-2.8');
+  divColFullColHalf2.setAttribute('class', 'column-full column-half mb-2.8');
+  img.setAttribute('src', data.entries[i].photo);
+  h2.setAttribute('class', 'mb-0');
+
+  h2.textContent = data.entries[i].title;
+  p.textContent = data.entries[i].notes;
+
+  // h2.textContent = data.entries.title;
+  // p.textContent = data.entries.notes;
+
+  divRow.appendChild(divColFullColHalf);
+  divColFullColHalf.appendChild(img);
+  divRow.appendChild(divColFullColHalf2);
+  divColFullColHalf2.appendChild(divRow2);
+  divRow2.appendChild(h2);
+  divColFullColHalf2.appendChild(divRow3);
+  divRow3.appendChild(p);
+
+  return divRow;
+}
+
+var $list = document.querySelector('ul');
+
+for (var i = 0; i < data.entries.length; i++) {
+  $list.appendChild(renderEntry(data.entries[i]));
+  // console.log('data entries', data.entries);
+  // console.log('data entries title', data.entries[i].title);
+}
