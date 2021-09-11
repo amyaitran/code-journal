8e8bba4fb2b41a055bed1f3023aa1373ecbab6d0
Amy Tran <amyaitran@gmail.com>
2021-09-10 17:16:37 -0700

    added edit icon, opens pre-populated entry form when edit icon is clicked

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@
 var $photoimg = document.querySelector('#entry-img');
 var $photoURL = document.querySelector('#photo');
 var $form = document.querySelector('#input-form');
-var $entriesPage = document.querySelector('#entries-page');
-var $entriesFormPage = document.querySelector('#entry-form');
 var $noEntriesText = document.querySelector('#no-entries-text');
 var $list = document.querySelector('ul');
 var $viewElements = document.querySelectorAll('.view');
 var $newButton = document.querySelector('#new-btn');
 var $navEntries = document.querySelector('#navEntries');
+var $editTitle = document.querySelector('#title');
+var $editNotes = document.querySelector('#notes');
 
 $photoURL.addEventListener('input', handleInput);
 $form.addEventListener('submit', handleSubmit);
@@ -52,15 +52,16 @@ function handleSubmit(event) {
   var photo = event.target.photo.value;
   var notes = event.target.notes.value;
   var $entry = { title, photo, notes };
+
+  // if () {
   $entry.entryId = data.nextEntryId;
   data.nextEntryId++;
   data.entries.unshift($entry);
   $photoimg.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
   $list.prepend(renderEntry(data.entries[0]));
-  $entriesPage.className = 'page';
-  $entriesFormPage.className = 'page hidden';
-  $noEntriesText.className = 'hidden page column-full flex jc-center';
+  $noEntriesText.classList.add('hidden');
+  // }
   switchView('entry-form');
 }
 
@@ -73,14 +74,23 @@ function renderEntry(entry) {
   var img = document.createElement('img');
   var h2 = document.createElement('h2');
   var p = document.createElement('p');
+  var editIcon = document.createElement('i');
 
   divRow.setAttribute('class', 'row');
-  divRow2.setAttribute('class', 'row');
+  divRow2.setAttribute('class', 'row pos-rel');
   divRow3.setAttribute('class', 'row');
   divColFullColHalf.setAttribute('class', 'column-full column-half mb-2.8');
   divColFullColHalf2.setAttribute('class', 'column-full column-half mb-2.8');
   img.setAttribute('src', entry.photo);
-  h2.setAttribute('class', 'mb-0');
+  img.setAttribute('class', 'entry');
+  h2.setAttribute('class', 'entry mb-0');
+  editIcon.setAttribute('class', 'entry fas fa-pen purple pos-abs');
+
+  img.setAttribute('data-entry-id', entry.entryId);
+  h2.setAttribute('data-entry-id', entry.entryId);
+  p.setAttribute('data-entry-id', entry.entryId);
+  editIcon.setAttribute('data-entry-id', entry.entryId);
+  divRow.setAttribute('data-entry-id', entry.entryId);
 
   h2.textContent = entry.title;
   p.textContent = entry.notes;
@@ -90,8 +100,23 @@ function renderEntry(entry) {
   divRow.appendChild(divColFullColHalf2);
   divColFullColHalf2.appendChild(divRow2);
   divRow2.appendChild(h2);
+  divRow2.appendChild(editIcon);
   divColFullColHalf2.appendChild(divRow3);
   divRow3.appendChild(p);
 
   return divRow;
+}
+
+var $ul = document.querySelector('ul');
+$ul.addEventListener('click', handleEdit);
+
+function handleEdit(event) {
+  if (event.target.getAttribute('class') === 'entry fas fa-pen purple pos-abs') {
+    var $entryId = event.target.getAttribute('data-entry-id');
+    switchView('entries');
+    $photoimg.setAttribute('src', (data.entries[data.entries.length - $entryId].photo));
+    $photoURL.value = data.entries[data.entries.length - $entryId].photo;
+    $editTitle.value = data.entries[data.entries.length - $entryId].title;
+    $editNotes.textContent = data.entries[data.entries.length - $entryId].notes;
+  }
 }
